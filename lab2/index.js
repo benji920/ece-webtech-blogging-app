ff8c42161e1ab059765ee138f6da50f70d2f5f01
benji920 <leskids92@gmail.com>
2022-10-11 15:35:39 +0200

    chore(lab2): format code

--- lab2/index.js
+++ lab2/index.js
@@ -1,8 +1,5 @@
-// Import a module
 const http = require('http')
-
 const handles = require('./handles')
   
-  const server = http.createServer(handles.serverHandle);
-  server.listen(8080)
-//
+const server = http.createServer(handles.serverHandle);
+server.listen(8080)
