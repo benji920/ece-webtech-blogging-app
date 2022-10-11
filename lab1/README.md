68a99cd463a288062e39844548b749907a694c49
benji920 <leskids92@gmail.com>
2022-10-11 14:38:33 +0200

    change folders name

---
+++ lab1/README.md
@@ -0,0 +1,10 @@
+# Lab 2
+
+The lab starts a new Node.js project which serves as the basis for the following courses.
+
+# Usage
+
+Do
+npm run develop
+
+then, go to localhost:8080/.
---
+++ lab1/index.js
@@ -0,0 +1,8 @@
+// Import a module
+const http = require('http')
+
+const handles = require('./handles')
+  
+  const server = http.createServer(handles.serverHandle);
+  server.listen(8080)
+//
