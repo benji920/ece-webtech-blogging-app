112d227aa0357be1c91bd2958f402276e995cb8b
benji920 <leskids92@gmail.com>
2022-10-17 23:58:47 +0200

    feat(lab04): Create website skeleton

---
+++ lab4/app/pages/about/index.js
@@ -0,0 +1,10 @@
+
+
+export default function About() {
+  return (
+    <div>
+      <h1>Lab04 of web-tech</h1>
+      you can see a list of albums in the article page
+    </div>
+  )
+}
---
+++ lab4/app/pages/contacts/index.js
@@ -0,0 +1,9 @@
+export default function Contacts() {
+  return (
+    <div>
+      <h2>contact ECE</h2>
+      <p>Paris</p>
+      <p>Benjamin DAVID</p>
+    </div>
+  )
+}
