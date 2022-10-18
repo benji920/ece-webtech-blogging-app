849692d471d230d6f6fe6a42b75246dba13db8a3
benji920 <leskids92@gmail.com>
2022-10-18 15:42:33 +0200

    feat(lab05): simple usage of tailwind

---
+++ lab5/app/components/Footer.js
@@ -0,0 +1,14 @@
+
+export default function Header(){
+  return (
+    <footer>
+      <a
+        href="https://www.adaltas.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by Adaltas
+      </a>
+    </footer>
+  )
+}
---
+++ lab5/app/components/Layout.js
@@ -0,0 +1,15 @@
+import Header from '../components/Header.js'
+import Footer from '../components/Footer.js'
+
+
+export default function Layout({children}){
+  return (
+    <div>
+      <Header />
+      <main>
+        {children}
+      </main>
+      <Footer />
+    </div>
+  )
+}
