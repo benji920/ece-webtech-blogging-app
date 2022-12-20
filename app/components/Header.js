b92e8320210e6cac4f88d96f1fa8a64b50cb430e
robin <vdRobin>
2022-12-20 12:37:54 +0100

    style: Adding a new navbar

--- app/components/Header.js
+++ app/components/Header.js
@@ -9,38 +9,38 @@ import Switch from "./Switch";
 export default function Header() {
   const { toggleDarkMode, darkMode } = useContext(DarkModeContext);
   return (
-    <header className="flex bg-slate-200 dark:bg-slate-800 px-10 py-2">
-      <Link href={`/`} className="flex-grow flex items-center">
+    <header className="flex space-x-4 bg-malibu-300 dark:bg-malibu-900 px-15 py-5">
+      <Link href={`/`} className="flex-grow py-1 px-5 flex items-center rounded">
         <Image
-          src="/adaltas.svg"
-          className="cursor-pointer h-6 mr-5"
+          src="/logo3.png"
+          className="cursor-pointer"
           alt="Adaltas Logo"
-          width={25}
-          height={25}
+          width={300}
+          height={100}
         />
-        <span className="rounded py-1 px-2 hover:bg-slate-600 hover:text-slate-100 dark:text-white">
-          Web technologies
-        </span>
+        {/* <span className="rounded py-1 px-2 text-malibu-800 hover:bg-malibu-400 hover:text-malibu-900 dark:text-malibu-100">
+        Web technologies
+      </span> */}
       </Link>
-      <ul className="flex gap-5">
-        <li className="rounded py-1 px-2 hover:bg-slate-600 hover:text-slate-100 dark:text-white">
+      <ul className="flex justify-left text-2xl items-center w-screen">
+        <li className="rounded py-1 px-5 text-malibu-800 hover:bg-malibu-400 hover:text-malibu-900 hover:font-medium  dark:text-malibu-200">
           {" "}
           <Link href="/articles">Articles</Link>{" "}
         </li>
-        <li className="rounded py-1 px-2 hover:bg-slate-600 hover:text-slate-100 dark:text-white">
+        <li className="rounded py-1 px-5 text-malibu-800 hover:bg-malibu-400 hover:text-malibu-900 hover:font-medium dark:text-malibu-200">
           <Link href="/about">About us</Link>
         </li>
-        <li className="rounded py-1 px-2 hover:bg-slate-600 hover:text-slate-100 dark:text-white">
+        <li className="rounded py-1 px-5 text-malibu-800 hover:bg-malibu-400 hover:text-malibu-900 hover:font-medium dark:text-malibu-200">
           <Link href="/contact">Contact us</Link>
         </li>
-        <li className="rounded py-1 px-2 hover:bg-slate-600 hover:text-slate-100 dark:text-white">
+      </ul>
+      <ul className="flex justify-right space-x-4 items-center text-2xl px-5">
+        <li className="rounded text-malibu-800 hover:bg-malibu-400 hover:text-malibu-900 hover:font-medium dark:text-malibu-200">
           <Login />
         </li>
-        <li>
-          <div>
-            <Switch isOn={darkMode} handleToggle={toggleDarkMode} />
-          </div>
-        </li>
+        <div className="flex justify-right space-x-4 items-center px-5">
+          <Switch isOn={darkMode} handleToggle={toggleDarkMode} />
+        </div>
       </ul>
     </header>
   );
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,7 +6,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'malibu': {
+        '50': '#f1fafe',
+        '100': '#e2f3fc',
+        '200': '#bee8f9',
+        '300': '#68cdf2',
+        '400': '#43c2ed',
+        '500': '#1babdc',
+        '600': '#0e89bb',
+        '700': '#0c6e98',
+        '800': '#0e5d7e',
+        '900': '#124d68',
+      },
+    },
   },
   plugins: [
     require('tailwindcss-font-inter'),
