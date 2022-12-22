27729d294daf3ba25f4800253a97be4409357c2f
robin <vdRobin>
2022-12-22 15:20:58 +0100

    feat: adding good colors

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function Footer() {
   return (
-    <footer className="flex bg-malibu-300 justify-center items-center dark:bg-malibu-900 px-15 py-5">
+    <footer className="flex bg-color300 justify-center items-center dark:bg-color900 px-15 py-5">
         <div className="flex  space-x-6 justify-center items-center px-5">
           <a href="https://www.facebook.com/zuck/" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
             <svg className="w-10 h-10" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
--- app/components/Header.js
+++ app/components/Header.js
@@ -9,7 +9,7 @@ import Switch from "./Switch";
 export default function Header() {
   const { toggleDarkMode, darkMode } = useContext(DarkModeContext);
   return (
-    <header className="flex space-x-4 bg-malibu-300 dark:bg-malibu-900 px-15 py-5">
+    <header className="flex space-x-4 bg-color300 dark:bg-color900 px-15 py-5">
       <Link href={`/`} className="flex-grow py-1 px-5 flex items-center rounded">
         <Image
           src="/logo3.png"
@@ -18,24 +18,24 @@ export default function Header() {
           width={300}
           height={100}
         />
-        {/* <span className="rounded py-1 px-2 text-malibu-800 hover:bg-malibu-400 hover:text-malibu-900 dark:text-malibu-100">
+        {/* <span className="rounded py-1 px-2 text-color800 hover:bg-color400 hover:text-color900 dark:text-color100">
         Web technologies
       </span> */}
       </Link>
       <ul className="flex justify-left text-2xl items-center w-screen">
-        <li className="rounded py-1 px-5 text-malibu-800 hover:bg-malibu-400 hover:text-malibu-900 hover:font-medium  dark:text-malibu-200">
+        <li className="rounded py-1 px-5 text-color800 hover:bg-color400 hover:text-color900 hover:font-medium  dark:text-color200">
           {" "}
           <Link href="/articles">Articles</Link>{" "}
         </li>
-        <li className="rounded py-1 px-5 text-color900 hover:bg-color50 hover:text-malibu-900 hover:font-medium dark:text-malibu-200">
+        <li className="rounded py-1 px-5 text-color900 hover:bg-color50 hover:text-color900 hover:font-medium dark:text-color200">
           <Link href="/about">About us</Link>
         </li>
-        <li className="rounded py-1 px-5 text-malibu-800 hover:bg-malibu-400 hover:text-malibu-900 hover:font-medium dark:text-malibu-200">
+        <li className="rounded py-1 px-5 text-color800 hover:bg-color400 hover:text-color900 hover:font-medium dark:text-color200">
           <Link href="/contact">Contact us</Link>
         </li>
       </ul>
       <ul className="flex justify-right space-x-4 items-center text-2xl px-5">
-        <li className="rounded text-malibu-800 hover:bg-malibu-400 hover:text-malibu-900 hover:font-medium dark:text-malibu-200">
+        <li className="rounded text-color800 hover:bg-color400 hover:text-color900 hover:font-medium dark:text-color200">
           <Login />
         </li>
         <div className="flex justify-right space-x-4 items-center px-5">
