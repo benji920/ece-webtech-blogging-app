33be644be8976859767a0a8715aeb91e6d3d92c7
benji920 <leskids92@gmail.com>
2022-12-23 19:34:51 +0100

    ops: posted column added

--- app/pages/articles.js
+++ app/pages/articles.js
@@ -13,7 +13,10 @@ export default function Articles() {
   const supabase = useSupabaseClient();
   useEffect(() => {
     (async () => {
-      let { data, error, status } = await supabase.from("articles").select();
+      let { data, error, status } = await supabase
+        .from("articles")
+        .select()
+        .eq("posted", "true");
       setContacts(data);
     })();
   }, [supabase]);
@@ -116,16 +119,4 @@ export default function Articles() {
       </div>
     </Layout>
   );
-}
-
-export function relativeDays(timestamp) {
-  const rtf = new Intl.RelativeTimeFormat("en", {
-    numeric: "auto",
-  });
-  const oneDayInMs = 1000 * 60 * 60 * 24;
-  const daysDifference = Math.round(
-    (timestamp - new Date().getTime()) / oneDayInMs
-  );
-
-  return rtf.format(daysDifference, "day");
 }
